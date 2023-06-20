225de85f46e9abd705f375fae058c09b88a5ab2a
rtaladua <rtaladua@gmail.com>
2023-06-20 08:33:19 -0700

    Scoreboard

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 // Word bank
 const wordBank = ["doughnut", "Homer", "Bart", "Lisa", "Marge", "Krusty the Clown","Ned Flanders", "Milhouse", "Moe", "Mr Burns"];
 
-  
+// Scoreboard variables
+let wins = 0;
+let losses = 0; 
 
