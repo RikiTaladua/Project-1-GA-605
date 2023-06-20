7b27f41406288363d3fa4cc5f63a465c84097c4a
rtaladua <rtaladua@gmail.com>
2023-06-20 08:36:45 -0700

    generate random word from word bank

--- app.js
+++ app.js
@@ -5,3 +5,6 @@ const wordBank = ["doughnut", "Homer", "Bart", "Lisa", "Marge", "Krusty the Clow
 let wins = 0;
 let losses = 0; 
 
+// Choose a random word from the word bank
+let randomIndex = Math.floor(Math.random() * wordBank.length);
+let word = wordBank[randomIndex];
