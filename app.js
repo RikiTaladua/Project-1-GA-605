fc8b82fb6d3edb83011271c74055f401c9cdf877
rtaladua <rtaladua@gmail.com>
2023-06-20 08:55:44 -0700

    added main mechanics of the game

--- app.js
+++ app.js
@@ -33,3 +33,48 @@ const usedLetters = new Set();
 lettersElement.addEventListener("click", function(event) {
     if (event.target.tagName === "BUTTON") {
       const letter = event.target.textContent.toLowerCase();
+
+      // Check if the letter has already been used
+    if (usedLetters.has(letter)) {
+        event.target.disabled = true;
+        return;
+      }
+
+      // Add the letter to the set of used letters
+    usedLetters.add(letter);
+
+    event.target.disabled = true;
+    if (word.includes(letter)) {
+     // Replace underscores with the guessed letter
+     for (let i = 0; i < word.length; i++) {
+        if (word[i] === letter) {
+          wordArray[i] = letter;
+        }
+      }
+      wordElement.textContent = wordArray.join(" ");
+
+      if (!wordArray.includes("_")) {
+        // Player wins the game
+        spaceElement.textContent = "WIN!";
+        wins++;
+        updateScoreboard();
+      }
+    } else {
+      // Incorrect guess
+      event.target.classList.add("dark");
+      event.target.classList.add("used"); // Add 'used' class for styling
+      wrongGuesses++;
+
+      // Update the incorrect guess count and display
+      spaceElement.textContent = `Wrong Guesses: ${wrongGuesses}`;
+
+      if (wrongGuesses === 6) {
+        // Player loses the game
+        wordElement.textContent = word;
+        spaceElement.textContent = "LOSS!";
+        losses++;
+        updateScoreboard();
+      }
+    }
+  }
+});   
